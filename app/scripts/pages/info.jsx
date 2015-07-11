e53589b5f0f43f4df7b00c18b3cab7afa4fd612a
phillycoder <jmustaq@gmail.com>
2015-07-11 18:07:56 -0400

    test
    test

--- app/scripts/pages/info.jsx
+++ app/scripts/pages/info.jsx
@@ -7,9 +7,9 @@ class Info extends React.Component {
       <div>
         <h1>Info area</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aliquam, 
-          aspernatur possimus natus. Sapiente odio quibusdam eum sunt, velit vel saepe 
-          facere voluptas animi explicabo consequuntur provident id at quis.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aliquam,
+          aspernatur possimus natus. Sapiente odio quibusdam eum sunt, velit vel saepe
+          facere voluptas animi explicabo consequuntur provident id at quis.test
         </p>
       </div>
     );
